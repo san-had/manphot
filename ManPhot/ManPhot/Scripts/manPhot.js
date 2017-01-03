d478b8eebc250570501bdc5a9638715323342cfa
san-had <hadhazis@gmail.com>
2017-01-03 21:19:06 +0100

    Added ManPhot project

---
+++ ManPhot/ManPhot/Scripts/manPhot.js
@@ -0,0 +1,60 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+$(document).ready(function () {
+    manPhotNamespace.initialize();
+});
+
+(function () {
+    this.manPhotNamespace = this.manPhotNamespace || {};
+    var ns = this.manPhotNamespace;
+
+    ns.initialize = function () {
+        //$('#btnRefresh').on('click', ns.testLinearRegression);
+        $('#btnRefresh').on('click', ns.load_UU_Aur);
+    }
+
+    ns.load_UU_Aur = function () {
+        var vstar = starInstanceNameSpace.get_UU_Aur();
+        alert(vstar.name);
+        alert(vstar.comps[0].label);
+        //var hello = get_hello();
+        //alert(hello);
+    }
+
+    ns.testLinearRegression = function () {
+        var known_x = [5.020, 4.974, 6.016, 6.206, 6.483, 6.626];
+        var known_y = [-13.287, -13.349, -12.229, -12.063, -11.744, -11.592];
+
+        var lr = ns.linearRegression(known_y, known_x);
+        $('#slope').html(lr.slope);
+        $('#intercept').html(lr.intercept);
+        $('#r2').html(lr.r2);
+    }
+
+    ns.linearRegression = function (y, x) {
+
+        var lr = {};
+
+        var n = y.length;
+        var sum_x = 0;
+        var sum_y = 0;
+        var sum_xy = 0;
+        var sum_xx = 0;
+        var sum_yy = 0;
+
+        for (var i = 0; i < y.length; i++) {
+            sum_x += Number(x[i]);
+            sum_y += y[i];
+            sum_xy += (x[i] * y[i]);
+            sum_xx += (x[i] * x[i]);
+            sum_yy += (y[i] * y[i]);
+        }
+
+        lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+        lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
+        lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+        return lr;
+    }
+
+})();
---
+++ ManPhot/RotImg/Scripts/maprotate.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+$(document).ready(function () {
+    mapRotateNamespace.initialize();
+});
+
+(function () {
+    this.mapRotateNamespace = this.mapRotateNamespace || {};
+    var ns = this.mapRotateNamespace;
+
+    var degree = 0;
+
+    ns.initialize = function () {
+        $('#btnPlus').on('click', rotatePlus);
+        $('#btnMinus').on('click', rotateMinus);
+        $('#imgMap').on('click', rotatePlus);
+        $('#txtDegree').val(30);
+
+        var mapName = getQueryVariable("mapName");
+        if (mapName === undefined) {
+            mapName = "UU_Aur.png";
+        }
+
+        loadMapImage(mapName);
+    };
+
+    function rotatePlus() {
+        degree += Number($('#txtDegree').val());
+        $('#imgMap').css('transform', 'rotate(' + degree + 'deg)');
+    }
+
+    function rotateMinus() {
+        degree -= Number($('#txtDegree').val());
+        $('#imgMap').css('transform', 'rotate(' + degree + 'deg');
+    }
+
+    function loadMapImage(mapName) {
+        $("#imgMap").attr('src', "maps/" + mapName);
+    }
+
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                return pair[1];
+            }
+        }
+    }
+})();
